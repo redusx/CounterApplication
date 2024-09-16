b1e682372d4ca1c6bf97a2d33aadcfe09cb18066
redusx <149193181+redusx@users.noreply.github.com>
2024-09-17 02:39:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/model/CounterViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.model
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+
+
+class CounterViewModel : ViewModel() {
+    private val _count = mutableStateOf(value = 0)
+    val count:  MutableState<Int> = _count
+         fun increment() {
+            count.value++
+        }
+        fun decrement(){
+            count.value--
+        }
+
+
+}
